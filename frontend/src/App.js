5636d226443dca9a038df52e5c08c11df8586366
matheusprado1 <matheusprado1096@gmail.com>
2022-11-04 18:27:52 -0300

    feat: add routes in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Homescreens from './screen/Homescreens';
+import Homescreens from "./screen/Homescreens";
+import ProductScreen from "./screen/ProductScreen";
+import Auth from "./screen/Auth";
+import CartScreen from "./screen/CartScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Homescreens />
-      <Footer/>
+      <Router>
+        <Header />
+        <main>
+          <Container className="py-5">
+            <Routes>
+              <Route path="/" exact element={<Homescreens/>} />
+              <Route path="/product/:id" exact element={<ProductScreen/>} />
+              <Route path="/cart" exact element={<CartScreen/>} />
+              <Route path="/login" exact element={<Auth/>} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
