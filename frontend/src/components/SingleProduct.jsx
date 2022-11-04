da031b83eca5ded6d0b07e3939a913a7fc74c856
matheusprado1 <matheusprado1096@gmail.com>
2022-11-04 18:27:07 -0300

    feat:add Link in SingleProduct

--- frontend/src/components/SingleProduct.jsx
+++ frontend/src/components/SingleProduct.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import Rating from "./Rating";
+import { Link } from "react-router-dom";
 
 function SingleProduct({ product }) {
   return (
     <>
-      <Card>
-        <Card.Body>
-          <Card.Img src={product.image} />
-          <Card.Title>{product.name}</Card.Title>
-          <Card.Text>{product.desc}</Card.Text>
-          <Rating value={product.rating} />
-          <h2>R${product.price}</h2>
-        </Card.Body>
-      </Card>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Img src={product.image} />
+            <Link to={`/product/${product._id}`}><h1>{product.name}</h1></Link>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.desc}</Card.Text>
+            <Rating value={product.rating} />
+            <h2>R${product.price}</h2>
+          </Card.Body>
+        </Card>
+      </Col>
     </>
   );
 }
