2d624b2bd40fd6fde101a4470600a153c0ee3809
matheusprado1 <matheusprado1096@gmail.com>
2022-11-04 16:17:43 -0300

    feat: add SingleProduct component

---
+++ frontend/src/components/SingleProduct.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function SingleProduct({ product }) {
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Card.Img src={product.image} />
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>{product.desc}</Card.Text>
+          <h2>R${product.price}</h2>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default SingleProduct;
