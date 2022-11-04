6eab25089679d11c9cbf9cfb88421841681ee96a
matheusprado1 <matheusprado1096@gmail.com>
2022-11-04 16:18:29 -0300

    feat: doc with products

---
+++ frontend/src/products.jsx
@@ -0,0 +1,40 @@
+const Products= [
+    {
+        '_id':'1',
+        'name': 'produto 1',
+        'brand': 'marca 1',
+        'price': 100,
+        'desc': 'produto novo',
+        'rating': 4.1,
+        'image': '/images/produto1.jpg'
+    },
+    {
+        '_id':'2',
+        'name': 'produto 2',
+        'brand': 'marca 2',
+        'price': 200,
+        'desc': 'produto semi novo',
+        'rating': 4.3,
+        'image': '/images/produto2.jpg'
+    },
+    {
+        '_id':'3',
+        'name': 'produto 3',
+        'brand': 'marca 3',
+        'price': 300,
+        'desc': 'produto novo',
+        'rating': 3.1,
+        'image': '/images/produto3.jpg'
+    },
+    {
+        '_id':'4',
+        'name': 'produto 4',
+        'brand': 'marca 4',
+        'price': 400,
+        'desc': 'produto novo',
+        'rating': 2.1,
+        'image': '/images/produto4.jpg'
+    },
+]
+
+export default Products
